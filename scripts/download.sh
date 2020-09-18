38226181045693cec70455380c6f77aa67f5b24f
Steve Pederson <stephen.pederson@adelaide.edu.au>
2020-09-18 14:02:58 +0930

    Initial Commit

---
+++ scripts/download.sh
@@ -0,0 +1,7 @@
+WD=/hpcfs/users/a1018048/ENCODE_T47D_HiC_ENCSR549MGQ
+DEST=${WD}/data/raw
+cd $DEST
+wget -O ENCLB758KFU_R1.fastq.gz https://www.encodeproject.org/files/ENCFF639RSW/@@download/ENCFF639RSW.fastq.gz
+wget -O ENCLB758KFU_R2.fastq.gz https://www.encodeproject.org/files/ENCFF901GID/@@download/ENCFF901GID.fastq.gz
+wget -O ENCLB183QHG_R1.fastq.gz https://www.encodeproject.org/files/ENCFF887JSU/@@download/ENCFF887JSU.fastq.gz
+wget -O ENCLB183QHG_R2.fastq.gz https://www.encodeproject.org/files/ENCFF308DXR/@@download/ENCFF308DXR.fastq.gz
